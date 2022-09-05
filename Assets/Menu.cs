a7b98c5c17f0c15a8d66149264aafd3d6f8b11f8
Sylvester Salo <sylttysalo@gmail.com>
2022-09-05 15:21:34 +0300

    Menu perustoiminta
    Menu, jossa pelivalikko sekä kirjautumis- ja rekisteröintipohjat. Ei sisällä vielä kaikkia toiminnallisuuksia, kuten käyttäjän rekisteröintiä tietokantaan.

---
+++ Assets/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public void PlaySolo()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void PlayOnline()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
