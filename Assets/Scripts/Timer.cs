b59426e3d8611eae5f495c6340df7e2483dcea71
Jonathan <44569793+Jonanananas@users.noreply.github.com>
2022-09-10 17:36:18 +0300

    Add a timer and change the project's fixed timestep

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+    public static Timer Instance;
+    public long timeLimit = 30000L;
+    public string text { get; private set; }
+    public long timeLong { get; private set; }
+    public bool timerRunning { get; private set; }
+    public void StartTimer() {
+        if (timerRunning == false) {
+            timeLong = timeLimit;
+            timerRunning = true;
+        }
+    }
+    public void StopTimer() {
+        timerRunning = false;
+    }
+    void Awake() {
+        #region Singleton
+        if (Instance != null) {
+            Debug.LogWarning("More than one instance of " + this.GetType().Name + " found!");
+            return;
+        }
+        Instance = this;
+        #endregion
+    }
+    void Start() {
+        text = "5:00:00";
+    }
+    void FixedUpdate() {
+        if (timerRunning) {
+            timeLong--;
+            UpdateTimerText();
+        }
+    }
+    void UpdateTimerText() {
+        long minutes = (timeLong / 100L) / 60L;
+        long seconds = (timeLong / 100L) - (60L * minutes);
+        long fractions = timeLong % 100L;
+        text = minutes + ":" + seconds + "." + fractions;
+    }
+}
