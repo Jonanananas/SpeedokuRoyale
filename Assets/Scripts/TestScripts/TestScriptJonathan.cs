f6def816d6451ef0582f17b019589e44ae90a357
Jonathan <44569793+Jonanananas@users.noreply.github.com>
2022-09-18 20:57:30 +0300

    Add password hashing

--- Assets/Scripts/TestScripts/TestScriptJonathan.cs
+++ Assets/Scripts/TestScripts/TestScriptJonathan.cs
@@ -6,12 +6,12 @@ using UnityEngine;
 public class TestScriptJonathan : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
-
+        HashPassword.Hash("passssssswww");
     }
 
     // Update is called once per frame
     void Update() {
-        Timer.Instance.StartTimer();
-        Trace.Log(Timer.Instance.text);
+        // Timer.Instance.StartTimer();
+        // Trace.Log(Timer.Instance.text);
     }
 }
