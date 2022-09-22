362f3233f3d8fc9d044a4702252c6942c4032ed1
Jonathan <44569793+Jonanananas@users.noreply.github.com>
2022-09-22 12:56:13 +0300

    Add a script to remove missing scripts from selected scenes

---
+++ Assets/Scripts/Editor/RemoveMissingScriptsFromScenes.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class RemoveMissingScriptsFromScenes : ScriptableWizard {
+    public Object[] scenes;
+
+    [MenuItem("Tools/Remove Missing Scripts From Scenes")]
+    static void CreateWizard() {
+        ScriptableWizard.DisplayWizard("Edit Scenes", typeof(RemoveMissingScriptsFromScenes), "Run");
+    }
+
+    void OnWizardCreate() {
+        foreach (Object s in scenes) {
+            string path = AssetDatabase.GetAssetPath(s);
+
+            Scene openScene = EditorSceneManager.OpenScene(path);
+
+            Object[] sceneObjects = Object.FindObjectsOfType(typeof(GameObject));
+            foreach (object obj in sceneObjects) {
+                GameObject go = (GameObject)obj;
+                Debug.Log(go);
+                GameObjectUtility.RemoveMonoBehavioursWithMissingScript(go);
+            }
+            EditorSceneManager.SaveScene(openScene);
+        }
+    }
+}
