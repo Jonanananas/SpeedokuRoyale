af4efcfefa5c040a64d9dc141e83ea78718a7ed1
Jonathan <44569793+Jonanananas@users.noreply.github.com>
2022-10-14 19:00:01 +0300

    Fix profile data display

--- Assets/Scripts/UI/DisplayProfileData.cs
+++ Assets/Scripts/UI/DisplayProfileData.cs
@@ -9,9 +9,14 @@ public class DisplayProfileData : MonoBehaviour {
     [SerializeField] TextMeshProUGUI usernameTMP, victoriesTMP, personalBestTMP;
 
     void OnEnable() {
-        if (LocalPlayer.Instance.profile == null) return;
+        if (LocalPlayer.Instance.profile == null) {
+            usernameTMP.text = "Log in to see profile data!";
+            victoriesTMP.text = "";
+            personalBestTMP.text = "";
+            return;
+        }
         usernameTMP.text = LocalPlayer.Instance.profile.username;
-        victoriesTMP.text = LocalPlayer.Instance.profile.victories.ToString();
-        personalBestTMP.text = LocalPlayer.Instance.profile.bestScore.ToString();
+        victoriesTMP.text = "Games Won: " + LocalPlayer.Instance.profile.victories.ToString();
+        personalBestTMP.text = "Best Score: " + LocalPlayer.Instance.profile.bestScore.ToString();
     }
 }
