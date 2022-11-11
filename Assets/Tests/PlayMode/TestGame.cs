c70e2d18a922591b90627d5499372761501e6c36
Jonathan <44569793+Jonanananas@users.noreply.github.com>
2022-11-11 18:42:31 +0200

    Don't fail a test

--- Assets/Tests/PlayMode/TestGame.cs
+++ Assets/Tests/PlayMode/TestGame.cs
@@ -18,7 +18,7 @@ namespace TestGame {
             var gameObject = new GameObject();
 
             // Check if a play button exists
-            string name = "Play Buttonadw"; // Button Name
+            string name = "Play Button"; // Button Name
             GameObject startButton = GameObject.Find(name);
             Assert.NotNull(startButton);
 
