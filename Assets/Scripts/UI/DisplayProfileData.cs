23c675231adf4bd9cfe8cff75407e82f3fafd6ed
Jukaiku <kaikuu@protonmail.com>
2022-12-10 21:06:44 +0200

    More localization
    LocalizeStringEventin TableEntryReferenceä vaihtamalla sain lisää käännöksiä toimimaan oikein.

--- Assets/Scripts/UI/DisplayProfileData.cs
+++ Assets/Scripts/UI/DisplayProfileData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
-
+using UnityEngine.Localization.Components;
 using TMPro;
 
 public class DisplayProfileData : MonoBehaviour {
@@ -10,13 +10,16 @@ public class DisplayProfileData : MonoBehaviour {
 
     void OnEnable() {
         if (LocalPlayer.Instance.profile == null) {
-            usernameTMP.text = "Log in to see profile data!";
-            victoriesTMP.text = "";
-            personalBestTMP.text = "";
+            usernameTMP.gameObject.GetComponent<LocalizeStringEvent>().StringReference.TableEntryReference = "Login2C";
+            victoriesTMP.gameObject.GetComponent<LocalizeStringEvent>().StringReference.TableEntryReference = "Empty";
+            personalBestTMP.gameObject.GetComponent<LocalizeStringEvent>().StringReference.TableEntryReference = "Empty";
             return;
         }
+        victoriesTMP.gameObject.GetComponent<LocalizeStringEvent>().StringReference.TableEntryReference = "GamesWon";
+        personalBestTMP.gameObject.GetComponent<LocalizeStringEvent>().StringReference.TableEntryReference = "BestScore";
+
         usernameTMP.text = LocalPlayer.Instance.profile.username;
-        victoriesTMP.text = "Games Won: " + LocalPlayer.Instance.profile.victories.ToString();
-        personalBestTMP.text = "Best Score: " + LocalPlayer.Instance.profile.bestScore.ToString();
+        victoriesTMP.text = LocalPlayer.Instance.profile.victories.ToString();
+        personalBestTMP.text = LocalPlayer.Instance.profile.bestScore.ToString();
     }
 }
